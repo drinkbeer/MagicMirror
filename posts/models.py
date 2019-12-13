b1d574d8a3a1cf67d7c6d60711aadb320cba7035
Jason Chen <jason.chen@shopify.com>
2019-12-13 13:12:59 -0500

    Clear the database so we could update data for demo

--- posts/models.py
+++ posts/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=400)
-    detail = models.TextField()
+    detail = models.TextField(default='Empty details of the product')
     cover = models.ImageField(upload_to='images/')
     link = models.URLField(max_length=300)
     store = models.CharField(max_length=100)
