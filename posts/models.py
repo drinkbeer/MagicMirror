db169326afe410ea9d64512434f773cc0165d4f6
Jason Chen <jason.chen@shopify.com>
2019-12-13 13:02:18 -0500

    Update the forms and models to add link and store name

--- posts/models.py
+++ posts/models.py
@@ -5,8 +5,8 @@ class Post(models.Model):
     title = models.CharField(max_length=400)
     detail = models.TextField()
     cover = models.ImageField(upload_to='images/')
-    link = models.URLField(max_length=300, default='https://www.shopify.com/')
-    store = models.CharField(max_length=100, default='Shopify')
+    link = models.URLField(max_length=300)
+    store = models.CharField(max_length=100)
 
     def __str__(self):
         return self.title
