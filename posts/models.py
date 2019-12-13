11a070a96a36e4d1f4727b3a7af23821cd98ecc2
Jason Chen <jason.chen@shopify.com>
2019-12-13 12:03:58 -0500

    Add the link to the original products

--- posts/models.py
+++ posts/models.py
@@ -5,6 +5,8 @@ class Post(models.Model):
     title = models.CharField(max_length=400)
     detail = models.TextField()
     cover = models.ImageField(upload_to='images/')
+    link = models.URLField(max_length=300, default='https://www.shopify.com/')
+    store = models.CharField(max_length=100, default='Shopify')
 
     def __str__(self):
         return self.title
